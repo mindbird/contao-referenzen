61b3b622f6219601edd250b9ef2f8d5f5924ab75
Wusch <florian.otto@mindbird.de>
2016-10-10 13:53:21 +0200

    Fix language files

--- contao/languages/de/tl_reference_archive.php
+++ contao/languages/de/tl_reference_archive.php
@@ -3,25 +3,25 @@
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_company_archive']['title'] = array('Title');
+$GLOBALS['TL_LANG']['tl_reference_archive']['title'] = array('Title');
 
 
 /**
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_company_archive']['title_legend'] = 'Titel';
+$GLOBALS['TL_LANG']['tl_reference_archive']['title_legend'] = 'Titel';
 
 
 /**
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_company_archive']['new']    = array('Neues Archiv', 'Ein neues Archiv anlegen');
-$GLOBALS['TL_LANG']['tl_company_archive']['show']   = array('Details', 'Details von Element %s anzeigen');
-$GLOBALS['TL_LANG']['tl_company_archive']['edit']   = array('Bearbeiten', 'Bearbeite Element %s');
-$GLOBALS['TL_LANG']['tl_company_archive']['editheader']   = array('Archiveinstellungen bearbeiten', 'Einstellungen des Elementes %s bearbeiten');
-$GLOBALS['TL_LANG']['tl_company_archive']['cut']    = array('Verschieben', 'Verschiebe Element %s');
-$GLOBALS['TL_LANG']['tl_company_archive']['copy']   = array('Kopieren', 'Kopiere Element %s');
-$GLOBALS['TL_LANG']['tl_company_archive']['delete'] = array('Löschen', 'Lösche Element %s');
+$GLOBALS['TL_LANG']['tl_reference_archive']['new']    = array('Neues Archiv', 'Ein neues Archiv anlegen');
+$GLOBALS['TL_LANG']['tl_reference_archive']['show']   = array('Details', 'Details von Element %s anzeigen');
+$GLOBALS['TL_LANG']['tl_reference_archive']['edit']   = array('Bearbeiten', 'Bearbeite Element %s');
+$GLOBALS['TL_LANG']['tl_reference_archive']['editheader']   = array('Archiveinstellungen bearbeiten', 'Einstellungen des Elementes %s bearbeiten');
+$GLOBALS['TL_LANG']['tl_reference_archive']['cut']    = array('Verschieben', 'Verschiebe Element %s');
+$GLOBALS['TL_LANG']['tl_reference_archive']['copy']   = array('Kopieren', 'Kopiere Element %s');
+$GLOBALS['TL_LANG']['tl_reference_archive']['delete'] = array('Löschen', 'Lösche Element %s');
 
-$GLOBALS['TL_LANG']['tl_company_archive']['title'] = array('Titel', 'Titel des Archivs');
-$GLOBALS['TL_LANG']['tl_company_archive']['sort_order'] = array('Sortierung', '');
+$GLOBALS['TL_LANG']['tl_reference_archive']['title'] = array('Titel', 'Titel des Archivs');
+$GLOBALS['TL_LANG']['tl_reference_archive']['sort_order'] = array('Sortierung', '');
--- contao/languages/de/tl_reference_category.php
+++ contao/languages/de/tl_reference_category.php
@@ -3,23 +3,23 @@
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_company_category']['title'] = array('Title');
+$GLOBALS['TL_LANG']['tl_reference_category']['title'] = array('Title');
 
 
 /**
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_company_category']['title_legend'] = 'Titel';
+$GLOBALS['TL_LANG']['tl_reference_category']['title_legend'] = 'Titel';
 
 
 /**
  * Buttons
  */
-$GLOBALS['TL_LANG']['tl_company_category']['new']    = array('Neue Branche', 'Eine neue Branche anlegen');
-$GLOBALS['TL_LANG']['tl_company_category']['edit']   = array('Bearbeiten', 'Bearbeite Element %s');
-$GLOBALS['TL_LANG']['tl_company_category']['copy']   = array('Kopieren', 'Kopiere Element %s');
-$GLOBALS['TL_LANG']['tl_company_category']['delete'] = array('Löschen', 'Lösche Element %s');
+$GLOBALS['TL_LANG']['tl_reference_category']['new']    = array('Neue Branche', 'Eine neue Branche anlegen');
+$GLOBALS['TL_LANG']['tl_reference_category']['edit']   = array('Bearbeiten', 'Bearbeite Element %s');
+$GLOBALS['TL_LANG']['tl_reference_category']['copy']   = array('Kopieren', 'Kopiere Element %s');
+$GLOBALS['TL_LANG']['tl_reference_category']['delete'] = array('Löschen', 'Lösche Element %s');
 
-$GLOBALS['TL_LANG']['tl_company_category']['title'] = array('Titel', 'Name der Branche');
+$GLOBALS['TL_LANG']['tl_reference_category']['title'] = array('Titel', 'Name der Branche');
 
-$GLOBALS['TL_LANG']['tl_company_category']['category'] = array('Branche auswählen');
+$GLOBALS['TL_LANG']['tl_reference_category']['category'] = array('Branche auswählen');
