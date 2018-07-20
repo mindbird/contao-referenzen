971b4361a81d886398197783dca1221db993228e
Sven Rhinow <sven@sr-tag.de>
2018-07-20 14:34:27 +0200

    'only_featured' und ['check_page_referenz'] als optionsfelder zur Ausgabe im FrontendModul eingefuegt.  und noch ein paar sortierungs-Änderungen

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -1,5 +1,6 @@
 <?php
-$GLOBALS ['TL_DCA'] ['tl_module'] ['palettes'] ['reference_list'] = '{title_legend},name,headline,type;{archiv_legend},reference_archiv,reference_category,jumpTo,reference_random,reference_filter_disabled,numberOfItems,perPage,imgSize,referenceTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS ['TL_DCA'] ['tl_module'] ['palettes'] ['reference_list'] = '{title_legend},name,headline,type;{redirect_legend},jumpTo;{archiv_legend},reference_archiv;{filter_legend},reference_category,check_page_referenz,reference_filter_disabled,only_featured;{list_legend},numberOfItems,perPage,imgSize,reference_random;{template_legend},referenceTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS ['TL_DCA'] ['tl_module'] ['palettes'] ['reference_detail'] = '{title_legend},name,headline,type,referenceTpl;{image_legend},imgSize;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 
 $GLOBALS ['TL_DCA'] ['tl_module'] ['fields'] ['numberOfItems'] ['eval'] ['mandatory'] = false;
 $GLOBALS ['TL_DCA'] ['tl_module'] ['fields'] ['reference_archiv'] = array(
@@ -29,15 +30,6 @@ $GLOBALS ['TL_DCA'] ['tl_module'] ['fields'] ['reference_category'] = array(
     'sql' => "varchar(10) NOT NULL default ''"
 );
 
-$GLOBALS ['TL_DCA'] ['tl_module'] ['fields'] ['reference_random'] = array(
-    'label' => &$GLOBALS ['TL_LANG'] ['tl_module'] ['reference_random'],
-    'inputType' => 'checkbox',
-    'eval' => array(
-        'tl_class' => 'w50 m12'
-    ),
-    'sql' => "char(1) NOT NULL default ''"
-);
-
 $GLOBALS ['TL_DCA'] ['tl_module'] ['fields'] ['reference_filter_disabled'] = array(
     'label' => &$GLOBALS ['TL_LANG'] ['tl_module'] ['reference_filter_disabled'],
     'inputType' => 'checkbox',
@@ -47,7 +39,16 @@ $GLOBALS ['TL_DCA'] ['tl_module'] ['fields'] ['reference_filter_disabled'] = arr
     'sql' => "char(1) NOT NULL default ''"
 );
 
-$GLOBALS ['TL_DCA'] ['tl_module'] ['palettes'] ['reference_detail'] = '{title_legend},name,headline,type;{image_legend},imgSize;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS ['TL_DCA']['tl_module']['fields']['reference_random'] = array(
+    'label' => &$GLOBALS['TL_LANG']['tl_module']['reference_random'],
+    'inputType' => 'checkbox',
+    'eval' => array(
+        'tl_class' => 'w50 m12'
+    ),
+    'sql' => "char(1) NOT NULL default ''"
+);
+
+
 
 $GLOBALS ['TL_DCA'] ['tl_module'] ['fields']['referenceTpl'] = array
 (
@@ -56,4 +57,24 @@ $GLOBALS ['TL_DCA'] ['tl_module'] ['fields']['referenceTpl'] = array
     'options_callback' => array('Reference\Tables\ReferenceTables', 'getReferenceTemplates'),
     'eval' => array('includeBlankOption' => true, 'chosen' => true, 'tl_class' => 'w50'),
     'sql' => "varchar(64) NOT NULL default ''"
+);
+
+$GLOBALS ['TL_DCA'] ['tl_module'] ['fields']['only_featured'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_module']['only_featured'],
+    'exclude'                 => true,
+    'filter'                  => true,
+    'inputType'               => 'checkbox',
+    'eval'                    => array('tl_class'=>'w50 m12'),
+    'sql'                     => "char(1) NOT NULL default ''"
+);
+
+$GLOBALS ['TL_DCA'] ['tl_module'] ['fields']['check_page_referenz'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_module']['check_page_referenz'],
+    'exclude'                 => true,
+    'filter'                  => true,
+    'inputType'               => 'checkbox',
+    'eval'                    => array('tl_class'=>'w50 m12'),
+    'sql'                     => "char(1) NOT NULL default ''"
 );
