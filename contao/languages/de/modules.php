2589c0b2a32e57f9790d9563e3a399bd4a5f154d
Sven Rhinow <sven@sr-tag.de>
2018-07-20 14:41:16 +0200

    Frontend-Moduleinträge in tl_module.php verschoben und Uebersetzung fuer neue Felder eingefuegt

--- contao/languages/de/modules.php
+++ contao/languages/de/modules.php
@@ -12,10 +12,3 @@ $GLOBALS['TL_LANG']['MOD']['reference'] = array('Referenzen', '');
 $GLOBALS['TL_LANG']['FMD']['reference'] = array('Referenzen', '');
 $GLOBALS['TL_LANG']['FMD']['reference_list'] = array('Referenzen - Liste', '');
 $GLOBALS['TL_LANG']['FMD']['reference_detail'] = array('Referenzen - Details', '');
-
-$GLOBALS['TL_LANG']['tl_module']['referenceTpl'] = array('Referenzentemplate', '');
-$GLOBALS['TL_LANG']['tl_module']['reference_archiv'] = array('Archiv', 'Welches Archiv soll dargestellt werden?');
-$GLOBALS['TL_LANG']['tl_module']['reference_random'] = array('Zufällige Wiedergabe', 'Sollen die Elemte in zufälliger Reihenfolge ausgegeben werden?');
-$GLOBALS['TL_LANG']['tl_module']['reference_filter_disabled'] = array('Filter deaktivieren', 'Soll der Filter für dieses Modul deaktiviert werden?');
-
-$GLOBALS['TL_LANG']['tl_module']['archiv_legend'] = 'Referenzen';
---
+++ contao/languages/de/tl_module.php
@@ -0,0 +1,18 @@
+<?php
+
+//Fields
+$GLOBALS['TL_LANG']['tl_module']['referenceTpl'] = array('Referenzentemplate', '');
+$GLOBALS['TL_LANG']['tl_module']['reference_category'] = array('Kategorie', 'Wenn hier eine Kategorie zugeordnet wurde werden nur die zugehörenden Referenzen angezeigt.');
+$GLOBALS['TL_LANG']['tl_module']['reference_archiv'] = array('Archiv', 'Welches Archiv soll dargestellt werden?');
+$GLOBALS['TL_LANG']['tl_module']['reference_random'] = array('Zufällige Wiedergabe', 'Sollen die Elemte in zufälliger Reihenfolge ausgegeben werden?');
+$GLOBALS['TL_LANG']['tl_module']['only_featured'] = array('nur hervorgehobene', 'Wenn diese Checkbox aktiviert ist werden nur Referenzen angezeigt, die als "hervorgehoben markiert wurden."');
+$GLOBALS['TL_LANG']['tl_module']['check_page_referenz'] = array('Kategorie-Seiten-Referenzierung beachten', 'Wenn die aktuelle Seite zu einer Kategorie zugeordnet wurde, werden dann auch nur die Referenzen angezeigt die zu dieser Kategorie zugeordnet sind."');
+$GLOBALS['TL_LANG']['tl_module']['reference_filter_disabled'] = array('Filter-Ausgabe deaktivieren', 'Soll die Filter-Anzeige für dieses Modul deaktiviert werden?');
+
+//Massages
+$GLOBALS['TL_LANG']['tl_module']['no_references'] = 'Mit den ausgewählten Filterkriterien sind keine Einträge vorhanden.';
+
+//Legends
+$GLOBALS['TL_LANG']['tl_module']['archiv_legend'] = 'Archiv-Einstellungen';
+$GLOBALS['TL_LANG']['tl_module']['filter_legend'] = 'Filter-Einstellungen';
+$GLOBALS['TL_LANG']['tl_module']['list_legend'] = 'Listen-Einstellungen';
